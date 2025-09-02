3ee5e2dab8086c7c96ddfa332c771ef48b294d7d
Adlai さ ん <126016337+AdlaiSantopadre@users.noreply.github.com>
2025-09-02 20:21:38 +0200

    creato repo separato pre flutter_frontend

--- flutter_frontend/lib/config.dart
+++
@@ -1,6 +0,0 @@
-class AppConfig {
-  static const authBaseUrl =
-      String.fromEnvironment('AUTH_BASE_URL', defaultValue: 'http://192.168.1.232:8081/auth');
-  static const apiBaseUrl =
-      String.fromEnvironment('API_BASE_URL', defaultValue: 'http://192.168.1.232:8080');
-} 
